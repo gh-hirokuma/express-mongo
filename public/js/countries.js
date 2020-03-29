61c5bf3325f22e3a3465d8137db47d7270630999
hiroshige.negishi <hirosige1@gmail.com>
2020-03-29 18:16:48 +0700

    ok

--- public/js/countries.js
+++ public/js/countries.js
@@ -1,22 +1,23 @@
-
 //国選ばせる
 $(function() {
-  $.ajax({
-    type: "GET",
-    url: "/countries.json",
-    success: async function(data) {
-      for (country of data.data) {
-        const $option = $("<option>")
-          .val(country.name)
-          .text(country.name);
+  // $.ajax({
+  //   type: "GET",
+  //   url: "/countries.json",
+  //   success: async function(data) {
+  //     for (country of data.data) {
+  //       const $option = $("<option>")
+  //         .val(country.name)
+  //         .text(country.name);
 
-        $('select[name="country"]').append($option);
-      }
+  //       $('select[name="country"]').append($option);
+  //     }
 
-      $(".js-example-basic-single").select2();
-    },
-    fail: function(err) {
-      console.log(err);
-    }
-  });
+  //     $(".js-example-basic-single").select2();
+  //   },
+  //   fail: function(err) {
+  //     console.log(err);
+  //   }
+  // });
+
+  $(".js-example-basic-single").select2();
 });
